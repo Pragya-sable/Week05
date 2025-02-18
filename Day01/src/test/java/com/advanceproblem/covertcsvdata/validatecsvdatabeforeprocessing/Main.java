abe88bf5254aa1e3ab44383a8d2681d608e4144a
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-18 23:39:09 +0530

    Validate CSV data

---
+++ Day01/src/test/java/com/advanceproblem/covertcsvdata/validatecsvdatabeforeprocessing/Main.java
@@ -0,0 +1,8 @@
+package com.advanceproblem.covertcsvdata.validatecsvdatabeforeprocessing;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "D:/Capgemini Training/Employee.csv";
+        ValidateCSVDataExample.validateCSV(filePath);
+    }
+}
