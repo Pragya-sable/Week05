ea189db15accc58dbf94c3f38bfd06d912bf662c
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-18 23:28:22 +0530

    Search for a record in CSV

---
+++ Day01/src/main/java/com/intermediateproblems/searchforarecordincsv/Main.java
@@ -0,0 +1,7 @@
+package com.intermediateproblems.searchforarecordincsv;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SearchForARecordInCSVExample.searchRecord("D:/Capgemini Training/Employee.csv"));
+    }
+}
