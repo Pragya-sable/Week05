ea52986a5435d1b7f906da20cae82c7a55f4e17c
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-18 23:50:22 +0530

    Detect Duplicates in CSV

---
+++ Day01/src/main/java/com/advanceproblem/converjsontocsv/detectduplicate/Main.java
@@ -0,0 +1,8 @@
+package com.advanceproblem.converjsontocsv.detectduplicate;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "D:/Capgemini Training/Student.csv";
+        DetectDuplicateInCSVExample.detectDuplicates(filePath);
+    }
+}
